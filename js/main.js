e65c09ef7ae94ae09b713be4c7cf12fdd1cfa9fa
Dennis Hou <dennis@fillr.com>
2017-12-19 16:14:54 +1100

    Added dragLeave events

--- js/main.js
+++ js/main.js
@@ -1,11 +1,33 @@
 // dataTransfer.getData(format);
 function allowDrop(event) {
-  console.log("it works", event);
+  // console.log("it works", event);
   event.target.classList.add("ableDrop");
   event.preventDefault();
   
 }
 
-function drag(dragevent) {
-  dragevent.dataTransfer.setData("text/plain", "This is the text to drag");
+function drag(dragEvent) {
+  dragEvent.dataTransfer.setData("text", dragEvent.target.id);
+}
+
+function drop(dropEvent) {
+  dropEvent.preventDefault();
+  var data = dropEvent.dataTransfer.getData("text");
+  console.log("data is: ", data);
+  dropEvent.target.appendChild(document.getElementById(data));
+  
+}
+
+function dragFinishes(event) {
+  console.log("Drag finishes ", event);
+  event.target.classList.remove("ableDrop");
+  if (event.target.parentNode != null
+    && event.target.parentNode.classList.contains("ableDrop")) {
+      event.target.parentNode.classList.remove("ableDrop");
+    }
+  // event.target.classList.remove("ableDrop");
+}
+
+function dragLeaves(event) {
+  event.target.classList.remove("ableDrop");
 }
