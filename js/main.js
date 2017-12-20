c82ce8c58e14252ac6213acd23d5a3097ad3ba69
Dennis Hou <dennis@fillr.com>
2017-12-20 18:06:15 +1100

    Updated

--- js/main.js
+++ js/main.js
@@ -13,13 +13,17 @@ function drop(dropEvent) {
   dropEvent.preventDefault();
   var data = dropEvent.dataTransfer.getData("text");
   console.log("data is: ", data);
-  dropEvent.target.appendChild(document.getElementById(data));
+  var toBeDropped = document.getElementById(data);
+  var droppedOnField = dropEvent.target;
+  droppedOnField.appendChild(toBeDropped);
+  
+  extractTargetMetadata(droppedOnField);
+  
   
 }
 
 function dragFinishes(event) {
   console.log("Drag finishes ", event);
-  // event.target.classList.remove("ableDrop");
   if (event.target.parentNode != null
     && event.target.parentNode.classList.contains("ableDrop")) {
       event.target.parentNode.classList.remove("ableDrop");
@@ -29,4 +33,9 @@ function dragFinishes(event) {
 
 function dragLeaves(event) {
   event.target.classList.remove("ableDrop");
+}
+
+
+function extractTargetMetadata(target) {
+  console.log("extract!" , target);
 }
